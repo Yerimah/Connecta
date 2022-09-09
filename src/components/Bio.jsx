5e0f3b16f768d4da5abe777e55909686868ff3e2
Yerimah <danielyerimah@yahoo.com>
2022-09-09 13:57:24 +0100

    setup dexieJs and create object stores to persist data

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import getPhotoUrl from 'get-photo-url'
 import Profile from '../assets/profileIcon.svg'
+import { db } from '../dexie'
 
 const Bio = () => {
   const [userDetails, setUserDetails] = useState({
@@ -11,27 +12,40 @@ const Bio = () => {
   const [editFormIsOpen, setEditFormIsOpen] = useState(false)
   const [profilePhoto, setProfilePhoto] = useState(Profile)
 
-  const updateUserDetails = (e) => {
+useEffect(() => {
+  const setDataFromDb = async () => {
+    const userDetailsFromDb = await db.bio.get('info')
+    const profilePhotoFromDb = await db.bio.get('profilePhoto')
+    userDetailsFromDb && setUserDetails(userDetailsFromDb)
+    profilePhotoFromDb && setProfilePhoto(profilePhotoFromDb)
+  }
+
+  setDataFromDb()
+})
+
+  const updateUserDetails = async (e) => {
     e.preventDefault()
-    setUserDetails({
+    const objectData = {
       name: e.target.nameOfUser.value,
       about: e.target.aboutUser.value,
-    })
-
+    }
+    setUserDetails(objectData)
+    await db.bio.put(objectData, 'info')
     setEditFormIsOpen(false)
   }
 
   const updateProfilePhoto = async () => {
     const newProfilePhoto = await getPhotoUrl('#profilePhotoInput')
     setProfilePhoto(newProfilePhoto)
+    await db.bio.put(newProfilePhoto, 'profilePhoto')
   }
 
   const editButton = <button onClick={() => setEditFormIsOpen(true)}>Edit</button>
 
     const editForm = (
       <form onSubmit={(event) => updateUserDetails(event)}>
-        <input type="text" id='' name="nameOfUser" placeholder='Your name'/>
-        <input type="text" id='' name="aboutUser" placeholder='About you'/>
+        <input type="text" id='' name="nameOfUser" defaultValue={userDetails?.name} placeholder='Your name'/>
+        <input type="text" id='' name="aboutUser" defaultValue={userDetails?.about} placeholder='About you'/>
         <br />
         <button type='button' className='cancel-button' onClick={() => setEditFormIsOpen(false)}>Cancel</button>
         <button type='submit'>Save</button>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
-import Pexels4 from '../assets/pexels-4.jpg'
-import Pexels5 from '../assets/pexels-5.jpg'
-import Pexels6 from '../assets/pexels-6.jpg'
-import Pexels7 from '../assets/pexels-7.jpg'
+import getPhotoUrl from 'get-photo-url'
+import React, { useEffect, useState } from 'react'
+import { db } from '../dexie'
 
 const Gallery = () => {
+    const [allPhotos, setAllPhotos] = useState([])
+
+    // useEffect(() => {
+    //     const setPhotosFromDb = async () => {
+    //         const allPhotosFromDb = await db.gallery.get('images')
+    //         allPhotosFromDb && setAllPhotos(allPhotosFromDb)
+    //     }
+    //     setPhotosFromDb()
+    // })
+
+    const addPhoto = async () => {
+      const newPhoto = {
+        id: Date.now(),
+        url: await getPhotoUrl('#addPhotoInput')
+      }
+
+      setAllPhotos([newPhoto, ...allPhotos])
+    //   await db.gallery.put([newPhoto, ...allPhotos], 'images')
+    }
+
   return (
     <>
       <input type="file" name='photo' id='addPhotoInput' />
-      <label htmlFor='addPhotoInput'>
+      <label htmlFor='addPhotoInput' onClick={addPhoto}>
         <i className="add-photo-button fas fa-plus-square"></i>
       </label>
 
       <section className="gallery">
-        <div className="item">
-          <img src={Pexels4} className="item-image" alt='photo' />
-          <button className="delete-button">Delete</button>
-        </div>
-        <div className="item">
-          <img src={Pexels5} className="item-image" alt='photo' />
-          <button className="delete-button">Delete</button>
-        </div>
-        <div className="item">
-          <img src={Pexels6} className="item-image" alt='photo' />
-          <button className="delete-button">Delete</button>
-        </div>
-        <div className="item">
-          <img src={Pexels7} className="item-image" alt='photo' />
-          <button className="delete-button">Delete</button>
-        </div>
+        {allPhotos.map((photo) => (
+          <div className="item" key={photo.id}>
+           <img src={photo.url} className="item-image" alt='photo' />
+           <button className="delete-button">Delete</button>
+          </div>
+        ))}
+        
+        
       </section>
     </>
   )
